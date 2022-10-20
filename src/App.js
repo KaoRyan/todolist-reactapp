11b30084b2ada016ff1f0e9b0861db74df4d9f4b
KaoRyan <T10822758@live.yuntech.edu.tw>
2022-10-20 16:31:35 +0800

    調整 component 檔案結構，並測試CICD能否正常運作

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Form from "./Component/Form.jsx"
+import Form from "./Component/Form/Form.jsx"
 
 function App() {
   return (
--- src/Component/Form/Form.jsx
+++ src/Component/Form/Form.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import List from "./List.jsx"
+import List from "../List/List.jsx"
 
 import "./Form.css"
 
